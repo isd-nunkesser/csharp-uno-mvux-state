8bfed56739a5406b6332e684b53cb349ed6309b5
nunkesser <robin.nunkesser@hshl.de>
2026-04-27 09:08:55 +0200

    Unify counter interaction handlers

--- app/UnoMvuxStateDemo/MainPage.xaml.cs
+++ app/UnoMvuxStateDemo/MainPage.xaml.cs
@@ -2,8 +2,33 @@ namespace UnoMvuxStateDemo;
 
 public sealed partial class MainPage : Page
 {
+    private int _count;
+
     public MainPage()
     {
         this.InitializeComponent();
+    }
+
+    private void OnIncrementClicked(object sender, RoutedEventArgs e)
+    {
+        _count++;
+        UpdateCount();
+    }
+
+    private void OnDecrementClicked(object sender, RoutedEventArgs e)
+    {
+        _count--;
+        UpdateCount();
+    }
+
+    private void OnResetClicked(object sender, RoutedEventArgs e)
+    {
+        _count = 0;
+        UpdateCount();
+    }
+
+    private void UpdateCount()
+    {
+        CountText.Text = $"Count: {_count}";
     }
 }
